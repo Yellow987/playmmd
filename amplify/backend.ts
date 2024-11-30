c1943c02a84fdf2fec337846dac320d22099ad03
Yellow <daryldsouza123@gmail.com>
2024-11-29 21:10:48 -0800

    remove all backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -6,6 +6,4 @@ import { storage } from "./storage/resource";
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
-  data,
-  storage,
 });
