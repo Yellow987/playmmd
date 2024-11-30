69cb07e5fdf38069da00e1e43a45b383723fae16
Yellow <daryldsouza123@gmail.com>
2024-11-30 15:50:58 -0800

    update storage rules

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,8 +3,20 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "amplifyStorage",
   access: (allow) => ({
-    "models/*": [
-      allow.guest.to(['read']), // additional actions such as "write" and "delete" can be specified depending on your use case
+    'models/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+    'motions/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+    'stages/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
     ],
   }),
 });
